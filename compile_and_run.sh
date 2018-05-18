4b755b4d0b41dea2740fec69b18755c6be92d711
Unknown <brugnara.daniele@gmail.com>
2018-05-18 18:51:59 +0200

    Added mini bash script

---
+++ compile_and_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# a simple script for lazy people to compile ad run
+make clean
+make
+clear
+
+echo Launching program
+./my_prog
--- main.C
+++ main.C
@@ -64,7 +64,7 @@ int main(int argc, char ** argv ){
     Process processor;
     
     //Set here the maximum number of events read and processed from the file
-    const int MAX_EVENTS=500000;
+    const int MAX_EVENTS=5000000;
     int good_events=0;
     
     //Program start
